3c91b9257f634d7f8b297d6c833ea0744a2993ca
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-10-14 09:22:31 -0300

    update

--- src/components/VirtualizedList/VirtualizedList.tsx
+++ src/components/VirtualizedList/VirtualizedList.tsx
@@ -6,24 +6,25 @@ interface VirtualizedListProps<DataType> {
 }
 export function VirtualizedList<DataType>({ data, renderItem }: VirtualizedListProps<DataType>) {
   const containerRef = React.useRef();
+  const threshold = 10;
   const itemHeight = 20;
   const [offset, setOffset] = React.useState(0);
-  const [screenLimit, setScreenLimit] = React.useState(10);
+  const [screenLimit, setScreenLimit] = React.useState(10 + threshold);
   const limit = offset + screenLimit;
 
   React.useLayoutEffect(() => {
     const $container = containerRef.current as HTMLUListElement;
     const windowHeight = window.innerHeight;
     
-    setScreenLimit(Math.ceil(windowHeight / itemHeight));
+    setScreenLimit(Math.ceil(windowHeight / itemHeight) + threshold);
     
     const containerMaxHeight = data.length * itemHeight;
     $container.style.height = `${containerMaxHeight}px`;
 
     function scrollHandler() {
       const scrollPosition = window.scrollY;
-      const offset = Math.floor(scrollPosition / itemHeight);
-      
+      const currentOffset = Math.floor(scrollPosition / itemHeight) - threshold;
+      const offset = Math.max(0, currentOffset);
       setOffset(offset);
     }
 
